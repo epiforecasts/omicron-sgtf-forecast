856535fef3a2691cd489dc3ff375130f30046492
kathsherratt <kathsherratt@gmail.com>
2021-12-14 15:24:19 +0000

    update

--- code/build-models.R
+++ code/build-models.R
@@ -1,4 +1,4 @@
-# Fit two models
+# Fit two-strain model with fixed and varying relationship between strains
 
 # Load packages
 library(here)
@@ -14,15 +14,15 @@ library(scoringutils)
 library(knitr)
 options(mc.cores = 4)
 
-# Load daily data, detrended (7-day moving average)
+# Load daily data
 source(here("code", "load-data.R"))
 timespan <- 1
 horizon <- 1
-obs <- daily_detrend
+obs <- daily
 
-# Cut off data before last 3 weeks and <3 days old
-start_date <- Sys.Date() - weeks(3)
-end_date <- Sys.Date() - 4
+# Cut off data before last 3 weeks and remove last day
+start_date <- max(obs$date) - weeks(3)
+end_date <- max(obs$date) - 1
 obs <- filter(obs, between(date, start_date, end_date))
 obs <- data.table(obs)
 
@@ -30,32 +30,37 @@ obs <- data.table(obs)
 source(here("code", "load-parameters.R"))
 variant_relationships <- c("scaled", "pooled")
 
-# Model with 1) scaled and 2) time-dependent relationship between variants
-forecast_fits <- map(variant_relationships,
-                     ~ forecast(obs,
-                                # scaled or pooled relationship
-                                variant_relationship = .x,
-                                # variant options
-                                voc_scale = parameters$voc_scale,
-                                scale_r = parameters$scale_r,
-                                strains = parameters$strains,
-                                r_init = parameters$r_init,
-                                overdispersion = parameters$overdispersion,
-                                timespan = timespan,
-                                horizon = horizon,
-                                voc_label = "Omicron",
-                                # processing options
-                                output_loglik = TRUE,
-                                adapt_delta = 0.99,
-                                max_treedepth = 15,
-                                refresh = 0,
-                                show_messages = FALSE))
-names(forecast_fits) <- variant_relationships
-saveRDS(forecast_fits, here("output", "forecast-fits.rds"))
+# Build models and save
+interactive <- FALSE
 
-# Unnest posterior
-forecasts <- map(forecast_fits,
-                 ~ unnest_posterior(.x) %>%
-                   mutate(across(mean:q95, round, 2)))
-saveRDS(forecasts, here("output", "forecasts.rds"))
+if (interactive) {
+  # Model with 1) scaled and 2) time-dependent relationship between variants
+  forecast_fits <- map(variant_relationships,
+                       ~ forecast(obs,
+                                  # scaled or pooled relationship
+                                  variant_relationship = .x,
+                                  # variant options
+                                  voc_scale = parameters$voc_scale,
+                                  scale_r = parameters$scale_r,
+                                  strains = parameters$strains,
+                                  r_init = parameters$r_init,
+                                  overdispersion = parameters$overdispersion,
+                                  timespan = timespan,
+                                  horizon = horizon,
+                                  voc_label = "Omicron",
+                                  # processing options
+                                  output_loglik = TRUE,
+                                  adapt_delta = 0.99,
+                                  max_treedepth = 15,
+                                  refresh = 0,
+                                  show_messages = FALSE))
+  names(forecast_fits) <- variant_relationships
+  saveRDS(forecast_fits, here("output", "forecast-fits.rds"))
+
+  # Unnest posterior
+  forecasts <- map(forecast_fits,
+                   ~ unnest_posterior(.x) %>%
+                     mutate(across(mean:q95, round, 2)))
+  saveRDS(forecasts, here("output", "forecasts.rds"))
+}
 
