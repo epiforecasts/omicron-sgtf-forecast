b1dc82e26e5cbaf4281e9820fe3c79ea1e44a5cf
kathsherratt <kathsherratt@gmail.com>
2021-12-16 16:23:47 +0000

    update data loading

--- sampling/model-sampling.R
+++ sampling/model-sampling.R
@@ -19,7 +19,8 @@ parameters$voc_scale <- c(0, 0.2)
 parameters$voc_label <- "SGT-result"
 variant_relationships <- "independent"
 
-# Load daily data and reshape to compare any SGT against SGT + NA-SGT
+# Load daily data, already filtered to specified dates
+# - reshape to compare any SGT against SGT + NA-SGT
 source(here("utils", "load-data.R"))
 daily_sgt <- daily_raw %>%
   transmute(date = date,
@@ -37,8 +38,8 @@ daily_sgt_detrend <- daily_sgt %>%
          seq_voc = zoo::rollmean(seq_voc, k = 7, align = "center", fill = NA),
          share_voc = seq_voc / seq_total)
 
-datasets <- list("data-raw" = daily_sgt,
-                 "data-smooth" = daily_sgt_detrend)
+datasets <- list("raw" = data.table(daily_sgt),
+                 "smooth" = data.table(daily_sgt_detrend))
 
 # Build models and save
 if (run_model) {
@@ -46,13 +47,7 @@ if (run_model) {
   for (data_type in names(datasets)) {
     save_to <- here("sampling", data_type, "fit")
 
-    # Cut off data before last 3 weeks and remove last day
     obs <- datasets[[data_type]]
-    start_date <- max(obs$date) - weeks(3)
-    end_date <- max(obs$date) - 1 # or 3: same date range for MA/raw data
-    obs <- filter(obs, between(date, start_date, end_date))
-    obs <- data.table(obs)
-
     # fit and save
     build_models(save_to, variant_relationships, parameters)
   }
