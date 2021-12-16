4370c13df75e8ef4f35cb98d95d16c1bc82dca79
kathsherratt <kathsherratt@gmail.com>
2021-12-16 16:23:47 +0000

    update data loading

--- transmission/model-transmission.R
+++ transmission/model-transmission.R
@@ -2,6 +2,7 @@
 
 # Sourcing from inside this script will build and save model
 if (!exists("run_model")) {run_model <- TRUE}
+if (!exists("data_type")) {data_type <- "data-raw"}
 
 # Load packages
 library(here)
@@ -17,10 +18,9 @@ library(scoringutils)
 library(knitr)
 options(mc.cores = 4)
 
-# Load daily data
+# Load daily data, already filtered to specified dates
 source(here("utils", "load-data.R"))
-# Format
-daily_sgtf <- daily_raw %>%
+daily_sgt <- daily_raw %>%
   transmute(date = date,
             cases = total_cases,
             cases_available = date,
@@ -28,27 +28,21 @@ daily_sgtf <- daily_raw %>%
             seq_voc = sgtf,
             share_voc = sgtf / total_sgt,
             seq_available = date)
+
 # Remove day-of-week in daily data for all cases (not applied to SGTF)
-daily_sgtf_detrend <- daily_sgtf %>%
+daily_sgt_detrend <- daily_sgt %>%
   mutate(cases = zoo::rollmean(cases, k = 7, align = "center", fill = NA),
          share_voc = seq_voc / seq_total) %>%
   filter(!is.na(cases))
 
-# Use raw data
-obs <- daily_sgtf
-
-# Cut off data before last 3 weeks and remove last day
-start_date <- max(obs$date) - weeks(3)
-end_date <- max(obs$date) - truncate_days
-obs <- filter(obs, between(date, start_date, end_date))
-obs <- data.table(obs)
+# Use specified data, raw or deseasonalised
+datasets <- list("data-raw" = data.table(daily_sgt),
+                 "data-smooth" = data.table(daily_sgt_detrend))
+obs <- datasets[[data_type]]
 
 # Load parameters
 source(here("utils", "load-parameters.R"))
 variant_relationships <- c("scaled", "correlated")
-
-datasets <- list("data-raw" = daily_sgtf,
-                 "data-smooth" = daily_sgtf_detrend)
 
 # Build models and save
 if (run_model) {
--- utils/load-data.R
+++ utils/load-data.R
@@ -1,4 +1,4 @@
-# Get SGTF data and clean
+# Get England SGTF data and clean
 library(here)
 library(readr)
 library(dplyr)
@@ -6,34 +6,70 @@ library(lubridate)
 library(zoo)
 library(ggplot2)
 
-# England
-data_path <- here("data", "private", "sgtf_onset_england.csv")
-date_type <- ifelse(grepl("onset", data_path), "onset", "specimen")
+# decisions:
+# - which dataset to use: by onset or specimen date
+date_type <- "specimen"
+# - length of dataset
+weeks_data <- 3
+# - truncation
+if (date_type == "onset") {
+  truncate_days <- 3
+} else {truncate_days <- 1}
 
-daily_raw <- read_csv(data_path, show_col_types = FALSE) %>%
-  rename(date = onsetdate) %>%
-  mutate(location = "England") %>%
+# Get data
+data_path <- here("data", "private",
+                  paste0("sgtf_",
+                         ifelse(date_type == "onset", date_type,
+                                "daily"),
+                         "_england.csv"))
+
+regional <- read_csv(data_path, show_col_types = FALSE) %>%
+  rename(date = grep("date", names(.), value = TRUE),
+         nhs_region = nhser_name,
+         sgtf_unknown = "NA") %>%
   rowwise() %>%
   mutate(total_sgt = sum(non_sgtf, sgtf, na.rm = TRUE),
-         total_cases = sum(non_sgtf, sgtf, `NA`, na.rm = TRUE)) %>%
-  ungroup()
+         total_cases = sum(non_sgtf, sgtf, sgtf_unknown, na.rm = TRUE)) %>%
+  ungroup() %>%
+  mutate(nhs_region = tidyr::replace_na(nhs_region, "Unknown"))
 
-truncate_days <- 3
+# Cut off data before last 3 weeks and remove truncation
+start_date <- max(regional$date) - weeks(weeks_data)
+end_date <- max(regional$date) - days(truncate_days)
+regional <- filter(regional,
+                    between(date, start_date, end_date))
+
+# england data
+england <- regional %>%
+  group_by(date) %>%
+  summarise(across(c(total_cases, sgtf_unknown, total_sgt, sgtf, non_sgtf),
+                     sum, na.rm = TRUE)) %>%
+  mutate(prop = sgtf / total_sgt,
+         nhs_region = "England")
+
+daily_raw <- bind_rows(england, regional)
+
+# --------------------------------
+#  Plot
+
+sgtf_fills <- c("non_sgtf" = "#c994c7", "sgtf" = "#dd1c77",
+           "sgtf_unknown" = "#e7e1ef")
 
 plot_daily_cases <- daily_raw %>%
-  filter(date >= max(date)-weeks(3) &
-           date <= max(date) - truncate_days) %>%
-  select(-c(prop, location)) %>%
-  rename(sgtf_unknown = `NA`) %>%
+  select(-c(prop)) %>%
   tidyr::pivot_longer(cols = c(non_sgtf, sgtf, sgtf_unknown),
                       names_to = "S-gene result") %>%
   ggplot(aes(x = date, y = value, fill = `S-gene result`)) +
   geom_col(position = "stack") +
-  geom_line(aes(y = total_cases)) +
+  geom_line(aes(y = total_cases), col = "grey 20") +
   labs(x = NULL, y = NULL,
-       caption = paste("Excludes most recent", truncate_days, "days' reports")) +
+       caption = paste("Data by", date_type,
+                       "date \n Excludes most recent",
+                       truncate_days, "days")) +
+  scale_fill_manual(values = sgtf_fills) +
   theme_bw() +
-  theme(legend.position = "bottom")
+  theme(legend.position = "bottom") +
+  facet_wrap(~ nhs_region, scales = "free")
 
 
 
