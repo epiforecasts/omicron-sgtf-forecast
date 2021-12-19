abe15d11c3722da25749e79dd9bfca6a861cadab
Sam Abbott <s.e.abbott12@gmail.com>
2021-12-19 17:33:59 +0000

    update data and process as random change for no reason...

--- R/load-public-data.R
+++ R/load-public-data.R
@@ -11,11 +11,11 @@ load_public_sgtf_data <- function() {
    raw_data_sgt <- read_csv(data_path_sgt)
 
    data_sgt <- raw_data_sgt %>%
-    mutate(date = ymd(specimen_date),
+    mutate(date = dmy(specimen_date),
            sgtf_type = recode(sgtf,
                               "Cases with confirmed S-gene" = "non_sgtf",
                               "Cases with confirmed SGTF" = "sgtf")) %>%
-    select(date, region = PHEC_name, sgtf_type, n, total_sgt = total) %>%
+    select(date, region = UKHSA_region, sgtf_type, n, total_sgt = total) %>%
     pivot_wider(id_cols = c(date, region, total_sgt),
                 names_from = sgtf_type, values_from = n) %>%
     mutate(sgtf = replace_na(sgtf, 0))
