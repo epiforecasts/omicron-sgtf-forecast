468b49611ac60ffe6a981faed94c252cc80ddcb8
kathsherratt <kathsherratt@gmail.com>
2021-12-21 21:51:31 +0000

    move summary plot to function

--- R/plot-summary.R
+++ R/plot-summary.R
@@ -1,54 +1,55 @@
-# Create a summary plot for each region
-# Key measures for the latest date before forecast
 # load packages
 library(ggplot2)
-library(scales)
 library(dplyr)
-library(here)
-library(readr)
 
-# omicron transmission advantage
-advantage <- simplify_posterior(sgtf$posterior, value_type = "voc_advantage") %>%
-  mutate(value_type = "Transmission advantage of Omicron")
+# load functions
+source(here("R", "load-local-data.R"))
+source(here("R", "plot-cumulative-percent.R"))
 
-# growth
-growth <- simplify_posterior(sgtf$posterior, value_type = "growth_from_rt") %>%
-  mutate(value_type = "Growth")
+plot_summary <- function(sgtf, bias, sgtf_cases,
+                         date_forecast_start, date_data_start) {
 
-# cumulative pop
-cumpercent <- simplify_posterior(sgtf$posterior, value_type = "cases") %>%
-  filter(type == "Omicron")
-cumpercent <- plot_cumulative_percent(cases = cases,
-                                      forecast_start = date_forecast_start,
-                                      data_start = date_data_start)$data %>%
-  mutate(value_type = "Omicron cumulative % population",
-         region = as.character(region))
+# get each output ---------------------------------------------------------
 
-# bias
-sampling <- simplify_posterior(bias$posterior, value_type = "voc_advantage") %>%
-  mutate(value_type = "Transmission advantage of S-gene result")
+  # omicron transmission advantage
+  advantage <- simplify_posterior(sgtf$posterior, value_type = "voc_advantage") %>%
+    mutate(value_type = "Omicron: transmission advantage")
 
-# join --------------------------------------------------------------------
-region_factor <- unique(sgtf$posterior$region)
-names(region_factor) <- region_factor
+  # growth
+  growth <- simplify_posterior(sgtf$posterior, value_type = "growth",
+                               variant_type = "Omicron") %>%
+    mutate(value_type = "Omicron: Growth rate")
 
-summary_data <- bind_rows(advantage, growth, cumpercent, sampling) %>%
-  filter(date == date_latest & !type %in% c("Combined", "non-Omicron")) %>%
-  select(region, value_type, type, median, q5, q95) %>%
-  mutate(across(median:q95, as.character))
+  # cumulative pop
+  cumpercent <- simplify_posterior(sgtf$posterior, value_type = "cases",
+                                   variant_type = "Omicron")
+  cumpercent <- plot_cumulative_percent(cases = sgtf_cases,
+                                        forecast_start = date_forecast_start,
+                                        data_start = date_data_start)$data %>%
+    mutate(value_type = "Omicron: cumulative population proportion",
+           region = as.character(region))
 
-summary_plot <- summary_data %>%
-  ggplot(aes(y = region)) +
-  geom_linerange(aes(xmin = q5, xmax = q95)) +
-  geom_point(aes(x = median)) +
-  facet_wrap(~ value_type, scales = "free_x") +
-  labs(y = NULL, x = NULL)
+  # bias
+  sampling <- simplify_posterior(bias$posterior, value_type = "voc_advantage") %>%
+    mutate(value_type = "Bias: transmission advantage of an S-gene result")
 
-forecast.vocs::plot_theme(summary_plot)
+  # join --------------------------------------------------------------------
+  region_factor <- as.character(sgtf$posterior$region)
+  region_factor <- unique(region_factor)
+  names(region_factor) <- region_factor
 
-# date at which Omicron accounts for 95% all cases
-voc_frac <- simplify_posterior(sgtf$posterior, value_type = "voc_frac")
-voc_frac <- plot_omicron_95(voc_frac = voc_frac,
-                            forecast_start = date_forecast_start,
-                            forecast_end = date_forecast_end)$data %>%
-  rename_with(~gsub(pattern = "q_", replacement = "", .))
+  summary_data <- bind_rows(advantage, growth, cumpercent, sampling) %>%
+    filter(date == date_latest & type %in% c("Omicron", "SGT-result")) %>%
+    mutate(region = recode_factor(region, !!!region_factor))
+
+  summary_plot <- summary_data %>%
+    ggplot(aes(y = region)) +
+    geom_linerange(aes(xmin = q5, xmax = q95)) +
+    geom_point(aes(x = median)) +
+    facet_wrap(~ value_type, scales = "free_x") +
+    labs(y = NULL, x = NULL)
+  summary_plot <- forecast.vocs::plot_theme(summary_plot) +
+    scale_x_continuous()
+
+  return(summary_plot)
+}
