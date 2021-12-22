cb36b7af68306626c9feddea30a2daee18c0cc2d
Sam Abbott <s.e.abbott12@gmail.com>
2021-12-22 14:14:27 +0000

    update readme and linerange of time at plot

--- R/plot-omicron-95.R
+++ R/plot-omicron-95.R
@@ -12,7 +12,7 @@ plot_omicron_95 <- function(voc_frac, forecast_start, forecast_end) {
 
   omicron_95 <- voc_frac %>%
     as_tibble() %>%
-    select(date, region, q_median = median, q5, q95) %>%
+    select(date, region, q_median = median, q5, q95, q20, q80) %>%
     pivot_longer(cols = starts_with("q"),
                  names_to = "quantile", values_to = "omicron_prop") %>%
     mutate(quantile = factor(quantile)) %>%
@@ -22,17 +22,17 @@ plot_omicron_95 <- function(voc_frac, forecast_start, forecast_end) {
     ungroup() %>%
     complete(quantile) %>%
     select(region, quantile, date) %>%
+    mutate(date = as.Date(ifelse(is.na(date), forecast_end, date),
+                        origin = lubridate::origin)) %>%
     pivot_wider(id_cols = region, names_from = quantile, values_from = date) %>%
     arrange(q_median) %>%
-    mutate(q5 = as.Date(ifelse(is.na(q5), forecast_end, q5),
-                        origin = lubridate::origin),
-           region = factor(region, ordered = TRUE)) %>%
+    mutate(region = factor(region, ordered = TRUE)) %>%
     filter(!is.na(region))
 
   plot_95_percent <- omicron_95 %>%
     ggplot(aes(y = region)) +
-    geom_linerange(aes(xmin = q5, xmax = q95)) +
-    geom_point(aes(x = q_median)) +
+    geom_linerange(aes(xmin = q5, xmax = q95), alpha = 0.3, size = 3) +
+    geom_linerange(aes(xmin = q20, xmax = q80), alpha = 0.3, size = 3) +
     geom_vline(xintercept = forecast_start, lty = 5, lwd = 1, col = "black") +
     labs(y = NULL, x = "Date when 95% of reported cases are Omicron")
 
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# Real-time estimation of the time-varying transmissibility advantage of Omicron in England using S-Gene Target Status as a Proxy
+# Real-time estimation of the time-varying transmission advantage of Omicron in England using S-Gene Target Status as a Proxy
 
 In this work, we use S-gene target failure (SGTF) as a proxy of variant status combined with reported case counts to explore the evidence for changes in transmission advantage over time for the Omicron variant. If present this could indicate the impact of immune escape, bias in SGTF data or differences in the populations within which the variants are circulating. We also report estimates for growth rates by variant and overall, case counts overall and by variant for a 14 day forecast window assuming constant future growth, the date at which Omicron will become dominant in England and in each NHS region, and the estimated cumulative percentage of the population with a reported Omicron case.
+
+Estimates are updated daily as new data is released but the summary of the situation is updated less frequently and so may not match current estimates. All data (both input and output) is available in this repository and the report should be fully reproducible. Reports and data as available at the time of manual releases (i.e when text is updated) are available from the [release page](https://github.com/epiforecasts/omicron-sgtf-forecast/releases).
 
 ## Output
 
