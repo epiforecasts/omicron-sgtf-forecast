d707e67b4b46f64fb15ddaf7116a2c41dc625e05
Sam Abbott <s.e.abbott12@gmail.com>
2021-12-23 23:43:29 +0000

    update region ordering whilst we are here

--- R/plot-cumulative-percent.R
+++ R/plot-cumulative-percent.R
@@ -8,12 +8,13 @@ source(here::here("R", "load-public-data.R"))
 plot_cumulative_percent <- function(cases_pop, forecast_start, data_start) {
 
   plot_cumulative_pop <- cases_pop %>%
+    mutate(region = factor(region, ordered = TRUE)) %>%
     ggplot(aes(x = date)) +
     geom_ribbon(aes(ymin = c_q5, ymax = c_q95), alpha = 0.3) +
     geom_ribbon(aes(ymin = c_q20, ymax = c_q80), alpha = 0.3) +
     geom_line(aes(y = c_median), alpha = 0.3, linetype = 2) +
     geom_vline(xintercept = forecast_start, lty = 5, lwd = 1, col = "black") +
-    scale_y_continuous(labels = scales::label_percent()) + #, limits = c(0,0.01)
+    scale_y_continuous(labels = scales::label_percent()) +
     facet_wrap(~ region) +
     labs(x = NULL,
          y = "Cumulative % of the population with a reported Omicron case")
