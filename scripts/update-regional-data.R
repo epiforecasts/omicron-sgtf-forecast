2cbafaca17ce34bb046180c912065100b67a1562
Sam Abbott <s.e.abbott12@gmail.com>
2021-12-28 20:46:02 +0000

    make data saving be by download date

--- scripts/update-regional-data.R
+++ scripts/update-regional-data.R
@@ -7,6 +7,8 @@ source(here::here("R", "plot-daily-cases.R"))
 daily <- load_data()
 
 date <- max(daily$date)
+download_date <- Sys.Date()
 readr::write_csv(
-  daily, here::here("data", "public", paste0(date, "-cases-by-sgtf.csv"))
+  daily,
+  here::here("data", "public", paste0(download_date, "-cases-by-sgtf.csv"))
 )
