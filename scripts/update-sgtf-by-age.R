ecd595a47317abc6663781b18380f262dc1a7e82
Sam Abbott <s.e.abbott12@gmail.com>
2022-01-05 16:36:35 +0000

    add cleaned up sgtf by age data

--- scripts/update-sgtf-by-age.R
+++ scripts/update-sgtf-by-age.R
@@ -1,11 +1,48 @@
+# This script requires the english_pillars.rds file to be present in
+# data/private
+# It takes this linelist and produces aggregated data by SGTF status, age and
+# region
 
+# Load packages
+library(dplyr)
+library(tidyr)
+library(readr)
+library(here)
 
-by_region_and_age <- english_pillars %>%
+# Load pillars data
+english_pillars <- readRDS(
+  here("data", "private", "english_pillars.rds")
+) %>%
+  as_tibble()
+
+# Summarise to aggregate counts by NHSE region
+sgtf_by_region_and_age <- english_pillars %>%
   filter(!is.na(age_group)) %>%
   select(-lower_age_limit, -positive, -total) %>%
-  group_by(pillar, week_infection, nhser_name, age_group) %>%
-  summarise_if(is.numeric, sum) %>%
+  group_by(pillar, date_specimen, nhser_name, age_group) %>%
+  summarise_if(is.numeric, sum, na.rm = TRUE) %>%
   ungroup() %>%
-  pivot_longer(starts_with("sgene"), names_to = "sgene_result",
-               values_to = "n") %>%
-  mutate(sgene_result = sub("^sgene_", "", sgene_result))
+  mutate(total = sgene_n_a + sgene_positive + sgene_negative,
+         total_sgt = sgene_positive + sgene_negative) %>%
+  select(-lft_flag, -negative, date = date_specimen, region = nhser_name,
+         age_group, total_cases = total, sgtf_unknown = sgene_n_a,
+         total_sgt, sgtf = sgene_negative, non_sgtf = sgene_positive)
+
+# Summarise for England
+sgtf_by_age <- sgtf_by_region_and_age %>%
+  group_by(pillar, date, age_group) %>%
+  summarise_if(is.numeric, sum, na.rm = TRUE) %>%
+  ungroup() %>%
+  mutate(region = "England")
+
+# Combined regions + national
+sgtf_by_region_and_age <- bind_rows(
+  sgtf_by_region_and_age, sgtf_by_age
+)
+
+# Filter for just pillar 2
+sgtf_by_region_and_age <- sgtf_by_region_and_age %>%
+  filter(pillar == 2)
+
+# Save as csv
+write_csv(here("data", "private", "sgtf-by-region-and-age.csv"))
