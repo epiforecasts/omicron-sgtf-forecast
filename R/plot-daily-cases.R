65e73ddda64d38a9aef2a4e4447e95ef9ba1064c
Sam Abbott <s.e.abbott12@gmail.com>
2022-01-10 23:32:44 +0000

    update models

--- R/plot-daily-cases.R
+++ R/plot-daily-cases.R
@@ -25,19 +25,24 @@ plot_daily_cases <- function(raw, caption,
   sgtf_fills <- c("Detected" = "#c994c7", "Failure" = "#dd1c77",
                   "Unknown" = "#e7e1ef")
 
-  raw %>%
+  plot <- raw %>%
     filter(date >= start_date) %>%
     pivot_longer(cols = contains("sgtf"), names_to = "S-gene result") %>%
     mutate("S-gene result" = factor(`S-gene result`,
                             levels = c("sgtf", "non_sgtf", "sgtf_unknown"),
                             labels = c("Failure", "Detected", "Unknown"))) %>%
     ggplot(aes(x = date, y = value, fill = `S-gene result`)) +
-    geom_col(position = "stack") +
-    geom_line(aes(y = total_cases), col = "grey 20") +
+    geom_col(position = "stack")
+  if (smooth_total) {
+    plot <- plot + 
+      geom_line(aes(y = total_cases), col = "grey 20")
+  }
+    plot <- plot +
     geom_vline(xintercept = as.Date(truncate_date), lty = 2) +
     labs(x = NULL, y = NULL, caption = caption) +
     scale_fill_manual(values = sgtf_fills) +
     scale_y_continuous(labels = scales::comma) +
     theme_bw() +
     theme(legend.position = "bottom")
+    return(plot)
 }
