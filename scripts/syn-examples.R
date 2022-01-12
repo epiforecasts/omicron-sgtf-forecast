819411b3bc07f2e6ccc42bbbd6f19a32e56ea4d0
Sam Abbott <s.e.abbott12@gmail.com>
2022-01-12 18:05:12 +0000

    2022-01-12 - update estimates

--- scripts/syn-examples.R
+++ scripts/syn-examples.R
@@ -5,6 +5,7 @@ library(purrr)
 
 # Load functions
 source(here("R", "R_growth_approx.R"))
+source(here("R", "simulations.R"))
 
 # r to r mapping with strength intervention
 r <- c(
@@ -17,20 +18,20 @@ scenarios <- CJ(
   alpha = c(0.6, 0.8, 1, 1.2, 1.4),
   G = c(4.6, 3.2),
   G_v = c(0.75, 1, 1.25),
-  k_v = c(0.75, 1, 1.25)
+  G_sd_v = c(0.75, 1, 1.25)
 )
 scenarios[, id := 1:.N]
 scenarios[, gt_type := ifelse(G == 4.6, "Intrinsic", "Household")]
-scenarios[, k := ifelse(G == 4.6, (3.1 / 4.6) ^ 2, (2.1 / 3.22) ^ 2)]
-
+scenarios[, G_sd := ifelse(G == 4.6, 3.1, 2.1)]
+scenarios[, G_sd_v := round(G_sd * G_sd_v, 1)][, G_v := round(G * G_v, 1)]
 # simulations
 sims <- map(
   split(scenarios, by = "id"),
   ~ data.table(
       time = 1:length(r),
       r = r,
-      r_sim = r_to_r_diff_gt(
-        r, .$G[[1]], .$k[[1]], .$G[[1]] * .$G_v[[1]], .$k[[1]] * .$k_v[[1]],
+      r_sim = r_to_r_diff_gt_sd_param(
+        r, .$G[[1]], .$G_sd[[1]], .$G_v[[1]], .$G_sd_v[[1]],
         .$alpha[[1]]
       )
     )
@@ -43,17 +44,3 @@ fwrite(
 )
 
 plot_syn_scenarios(sims[gt_type %in% "Household"])
-
-plot_syn_scenarios <- function(sims) {
-  
-}
-sims[gt_type %in% "Household"] |>
-  ggplot() +
-  aes(x = time, y = r_sim, col = factor(alpha), group = factor(alpha)) +
-  geom_hline(yintercept = 0, linetype = 2, col = "grey") +
-  geom_line() +
-  geom_line(aes(y = r), col = "black") +
-  theme_bw() +
-  theme(legend.position = "bottom") +
-  facet_grid(k_v ~ G_v) +
-  labs(x = "Day", y = "Daily growth rate", color = "Transmission advantage")
